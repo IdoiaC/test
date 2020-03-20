78df7274c4325c7b65fe97eebe6dcf5c01bd5e0e
IdoiaC <36922172+IdoiaC@users.noreply.github.com>
2020-03-20 16:39:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 // setup ports
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1234;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = 1234;
+var server_ip_address = '127.0.0.1';
 
 app.get('/', function(req, res) {
 
