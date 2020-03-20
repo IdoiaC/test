af10151f2472abd47197bc3ff9a6d7850bbbc2a0
IdoiaC <36922172+IdoiaC@users.noreply.github.com>
2020-03-20 17:19:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res) {
 
 });
 
+app.get('/downloadFile/', (req, res) => {
+  res.download('./demo.txt');
+});
+
 // server listens in on port
 app.listen(server_port, server_ip_address, function () {
 	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
