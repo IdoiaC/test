2a1719814eabf39fb7a99e37a2a1c6f90e9cd1b2
IdoiaC <36922172+IdoiaC@users.noreply.github.com>
2020-03-20 17:30:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
 
-// setup ports
+// ports setup
 var server_port = 8080;
 var server_ip_address = '0.0.0.0';
 
-app.get('/', function(req, res) {
-	 res.download('./demo.txt');
+// IMPORTANTE: he leído que estas variables ya están obsoletas y Openshift ya no las usa
+// cambiar a ip y puerto en los que sí pueda escuchar la app
+
+//var server_port = 8080 || 1234;
+//var server_ip_address = '10.129.4.181' || '127.0.0.1';
+
+app.get('/', function(req,res) {
+
+	res.end('Hello World!');
 });
 
 // server listens in on port
-app.listen(server_port, server_ip_address, function () {
-	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+app.listen(server_port, server_ip_address, function() {
+	console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });
+
+app.get('/downloadFile/', (req, res) => {
+  res.download('./demo.txt');
+})
