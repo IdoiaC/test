2dda1bc05ff87f84504269457e02873a1fc1ad9f
IdoiaC <36922172+IdoiaC@users.noreply.github.com>
2020-04-13 15:43:13 +0200

    buffer

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var server_port = 8080;
 var server_ip_address = '0.0.0.0';
 
 app.get('/:id', function(req,res) {
-// 	var buffer = Buffer.alloc(parseInt(req.params.id,10),5);
-	res.end("HIIIIII");
+ 	var buffer = Buffer.alloc(parseInt(req.params.id,10),5);
+	res.send(buffer);
 });
 
 // server listens in on port
