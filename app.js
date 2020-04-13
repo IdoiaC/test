ff953321714db10bddc6a7e1174cdbcdbc291b15
IdoiaC <36922172+IdoiaC@users.noreply.github.com>
2020-04-13 16:35:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/download/:id', function(req,res) {
 
 app.post('/upload',(req,res) => {
     let EDFile = req.files.file;
-    EDFile.mv(`./files/${EDFile.name}`,err => {
+    EDFile.mv(`./${EDFile.name}`,err => {
         if(err) return res.status(500).send({ message : err })
         	return res.status(200).send({ message : 'File upload' })
     });
