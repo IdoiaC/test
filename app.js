fc27ef9cb4fa90ef313a092183b89e188913a4db
IdoiaC <36922172+IdoiaC@users.noreply.github.com>
2020-04-13 16:35:06 +0200

    Upload files

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var express = require('express');
+const fileUpload = require('express-fileupload');
 var app = express();
 
 // ports setup
 var server_port = 8080;
 var server_ip_address = '0.0.0.0';
 
-app.get('/:id', function(req,res) {
+app.get('/download/:id', function(req,res) {
  	var buffer = Buffer.alloc(parseInt(req.params.id,10),5);
 	res.send(buffer);
+});
+
+app.post('/upload',(req,res) => {
+    let EDFile = req.files.file;
+    EDFile.mv(`./files/${EDFile.name}`,err => {
+        if(err) return res.status(500).send({ message : err })
+        	return res.status(200).send({ message : 'File upload' })
+    });
 });
 
 // server listens in on port
